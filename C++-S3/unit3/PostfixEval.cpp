814be8b557ff94550c745dcc561d25da2d1d0943
laxmaroon <102042850+laxmaroon@users.noreply.github.com>
2023-02-02 08:50:39 +0530

    C

--- C++-S3/unit3/PostfixEval.cpp
+++ C++-S3/unit3/PostfixEval.cpp
@@ -4,50 +4,51 @@
 
 using namespace std;
 
-// Function to evaluate postfix expression and return output
-int evaluatePostfix(string exp)
-{
-    stack<int> s;
+class EvaluatePostfix {
+    public:
+        int evaluate(string exp) {
+            stack<int> s;
 
-    // Scan all characters one by one
-    for (int i = 0; exp[i]; i++) {
+            // Scan all characters one by one
+            for (int i = 0; exp[i]; i++) {
+                // If the scanned character is an operand (number here),
+                // push it to the stack.
+                if (isdigit(exp[i]))
+                    s.push(exp[i] - '0');
 
-        // If the scanned character is an operand (number here),
-        // push it to the stack.
-        if (isdigit(exp[i]))
-            s.push(exp[i] - '0');
+                // If the scanned character is an operator, pop two
+                // elements from stack apply the operator
+                else {
+                    int val1 = s.top();
+                    s.pop();
+                    int val2 = s.top();
+                    s.pop();
 
-        // If the scanned character is an operator, pop two
-        // elements from stack apply the operator
-        else {
-            int val1 = s.top();
-            s.pop();
-            int val2 = s.top();
-            s.pop();
-
-            switch (exp[i]) {
-            case '+':
-                s.push(val2 + val1);
-                break;
-            case '-':
-                s.push(val2 - val1);
-                break;
-            case '*':
-                s.push(val2 * val1);
-                break;
-            case '/':
-                s.push(val2 / val1);
-                break;
+                    switch (exp[i]) {
+                    case '+':
+                        s.push(val2 + val1);
+                        break;
+                    case '-':
+                        s.push(val2 - val1);
+                        break;
+                    case '*':
+                        s.push(val2 * val1);
+                        break;
+                    case '/':
+                        s.push(val2 / val1);
+                        break;
+                    }
+                }
             }
+            return s.top();
         }
-    }
-    return s.top();
-}
+};
 
 // Driver code
 int main()
 {
+    EvaluatePostfix e;
     string exp = "231*+9-";
-    cout << evaluatePostfix(exp);
+    cout << e.evaluate(exp);
     return 0;
 }
